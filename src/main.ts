a3a31dc8677d0c57a9ced691b2b218f5030822f5
Anton Reshetov <reshetov.art@gmail.com>
2022-05-15 07:02:47 +0300

    feat: add styles (basic)

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import router from './router'
 import App from './App.vue'
 import { createPinia } from 'pinia'
+import './assets/scss/global.scss'
 
 createApp(App).use(router).use(createPinia()).mount('#app')
