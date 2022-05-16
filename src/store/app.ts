c332b0bab9e80ac315a298db2a24bb4e0217922c
Anton Reshetov <reshetov.art@gmail.com>
2022-05-16 15:46:15 +0300

    chore: set type

--- src/store/app.ts
+++ src/store/app.ts
@@ -2,9 +2,8 @@ import { defineStore } from 'pinia'
 import type { State } from './types/app'
 
 export const useAppStore = defineStore('app', {
-  state: () =>
-    ({
-      layout: 'Default',
-      isInit: false
-    } as State)
+  state: (): State => ({
+    layout: 'Default',
+    isInit: false
+  })
 })
