2dad1feca8994248f378272ab834290f85bb3254
Anton Reshetov <reshetov.art@gmail.com>
2023-11-02 16:09:48 +0300

    refactor: remove routing

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
-  <TheLayout>
-    <RouterView />
-  </TheLayout>
+  <div id="app">
+    <TheHeader />
+    <TheEditor />
+  </div>
 </template>
 
-<script setup lang="ts">
+<script setup lang="ts"></script>
 
-</script>
+<style lang="scss" scoped>
+#app {
+  display: grid;
+  grid-template-rows: var(--header-height) 1fr;
+  height: 100vh;
+}
+</style>
