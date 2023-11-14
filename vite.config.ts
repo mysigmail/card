67b653d81f0cb95e985cbdaebebd90c7cd412b1f
Anton Reshetov <reshetov.art@gmail.com>
2023-11-14 14:54:57 +0300

    build: add envs for base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
+/* eslint-disable node/prefer-global/process */
 import path from 'node:path'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
@@ -10,39 +11,40 @@ import { FileSystemIconLoader } from 'unplugin-icons/loaders'
 
 const pathSrc = path.resolve(__dirname, './src')
 
-export default defineConfig({
-  plugins: [
-    vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-      dts: `${pathSrc}/types/auto-imports.d.ts`,
-    }),
-    Components({
-      dts: `${pathSrc}/types/components.d.ts`,
-      resolvers: [
-        ElementPlusResolver(),
-        IconsResolver({
-          prefix: '',
-          customCollections: ['svg', 'unicons'],
-        }),
-      ],
-      dirs: ['src/layouts', 'src/components'],
-    }),
-    Icons({
-      customCollections: {
-        svg: FileSystemIconLoader('./src/assets/svg'),
-        unicons: FileSystemIconLoader(
-          './node_modules/@iconscout/unicons/svg/line',
-        ),
-      },
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': pathSrc,
-    },
-    dedupe: [
-      'vue',
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+
+  return defineConfig({
+    base: process.env.VITE_APP_BASE_PATH || '/',
+    plugins: [
+      vue(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+        dts: `${pathSrc}/types/auto-imports.d.ts`,
+      }),
+      Components({
+        dts: `${pathSrc}/types/components.d.ts`,
+        resolvers: [
+          ElementPlusResolver(),
+          IconsResolver({
+            prefix: '',
+            customCollections: ['svg', 'unicons'],
+          }),
+        ],
+        dirs: ['src/layouts', 'src/components'],
+      }),
+      Icons({
+        customCollections: {
+          svg: FileSystemIconLoader('./src/assets/svg'),
+          unicons: FileSystemIconLoader('./node_modules/@iconscout/unicons/svg/line'),
+        },
+      }),
     ],
-  },
-})
+    resolve: {
+      alias: {
+        '@': pathSrc,
+      },
+      dedupe: ['vue'],
+    },
+  })
+}
