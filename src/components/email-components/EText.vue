86a31719e3f196310121e4f0324b6bd9f2cd76ef
Anton Reshetov <reshetov.art@gmail.com>
2023-12-18 10:05:38 +0300

    feat: add text component

---
+++ src/components/email-components/EText.vue
@@ -0,0 +1,26 @@
+<template>
+  <MColumn
+    class="p-hover-tools"
+    :class="{
+      'p-edit-tool': editableId === id && editableToolName === group,
+    }"
+  >
+    <div v-html="value" />
+  </MColumn>
+</template>
+
+<script setup lang="ts">
+import { useComponentsStore } from '@/store/components'
+
+interface Props {
+  id: string
+  group: string
+  value?: string
+}
+
+defineProps<Props>()
+
+const { editableId, editableToolName } = useComponentsStore()
+</script>
+
+<style lang="scss" scoped></style>
